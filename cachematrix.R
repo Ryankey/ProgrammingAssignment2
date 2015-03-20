10ad85b725e27b6571e04b2d23396012f7d12ea6
Ryankey <ryankey721@gmail.com>
2015-03-20 17:09:41 -0400

    Initial function design
    Added definitions for makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix returns an object with a matrix and a cached inverse
+##
+## cacheSolve returns a cached inverse from object x or calculates the
+## inverse and caches the value in x while returning it
 
-## Write a short comment describing this function
+
+## makeCacheMatrix returns an object with a matrix and a cached inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    matrix.inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        matrix.inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) matrix.inverse <<- inverse
+    getinverse <- function() matrix.inverse
+    list(set = set, get = get,
+        setinverse = setinverse,
+        getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns a cached inverse from object x or calculates the
+## inverse and caches it while returning it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x' 
+    
+    matrix.inverse <- x$getinverse()
+    if(!is.null(matrix.inverse)){
+        message("getting cached inverse")
+        return(matrix.inverse)
+    }
+    data <- x$get()
+    matrix.inverse <- solve(data, ...)
+    x$setinverse(matrix.inverse)
+    matrix.inverse
 }
